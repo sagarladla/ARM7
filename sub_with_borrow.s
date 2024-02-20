db5f8718ae8fb9b0d640017a6b2b1cca195e1c15
Sagar Ladla <sagarladla@gmail.com>
2024-02-20 23:31:29 +0530

    indentation fix

--- sub_with_borrow.s
+++ sub_with_borrow.s
@@ -15,10 +15,10 @@ _start:
 	mov		r3,	#0x00			//	difference
 	mov		r4,	#0x00			//	borrow
 
-	ldr		r0,	=n1				//	r0 points to n1 ( *r0 = &n1 )
+	ldr		r0,	=n1			//	r0 points to n1 ( *r0 = &n1 )
 	ldr		r1,	[r0],	#4		//	r1 = n1, r0++
 	ldr		r2,	[r0],	#4		//	r2 <- *r0 and increment r0 4 bytes (.word size)
-	subs	r3,	r1,	r2			//	r3 = r1 - r2
+	subs		r3,	r1,	r2		//	r3 = r1 - r2
 	bcs		skip				//	branch if carry set to skip (carry flag is inverted if there is borrow)
 	add		r4,	#0x1
 skip:
@@ -27,8 +27,8 @@ skip:
 exit:
 	b		exit				//	`bal` branch always or we can use `b` (only brach)
 
-.org	0x108					//	origin address is 0x108
-n1:		.word	0x2
-n2:		.word	0x1
+.org	0x108						//	origin address is 0x108
+n1:	.word	0x2
+n2:	.word	0x1
 diff:	.word	0x00
 borrow:	.word	0x00
